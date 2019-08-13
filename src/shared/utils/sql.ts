6c652c8fe55ca61bf4585e9b027ec63f7ace8587
kuangshp <kuangshp@126.com>
2019-08-13 12:01:01 +0800

    新增插入语句的sql方法

--- src/shared/utils/sql.ts
+++ src/shared/utils/sql.ts
@@ -13,5 +13,25 @@ export const sqlParamsJoin = (data: { [propsName: string]: any }): string => {
       result.push(`${key}='${data[key]}'`);
     }
   });
-  return result.join(',');
+  return ` ${result.join(',')} `;
+};
+
+/**
+ * @param {type}
+ * @return:
+ * @Description: 拼接插入数据的sql语句
+ * @Author: 水痕
+ * @LastEditors: 水痕
+ * @Date: 2019-08-13 11:58:59
+ */
+export const sqlInsertJoin = (data: { [propsName: string]: any }): string => {
+  const keys = [];
+  const values = [];
+  Object.keys(data).forEach(key => {
+    if (data[key]) {
+      keys.push(key);
+      values.push(data[key]);
+    }
+  });
+  return ` (${keys.join(',')}) values (${values.join(',')}) `;
 };
