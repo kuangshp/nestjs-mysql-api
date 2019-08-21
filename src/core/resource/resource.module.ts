346350ce1668ad827220ff3fe3bc01eed5bb8df3
kuangshp <kuangshp@126.com>
2019-08-21 11:00:10 +0800

    修复bug

--- src/core/resource/resource.module.ts
+++ src/core/resource/resource.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ResourceService } from './resource.service';
 import { ResourceController } from './resource.controller';
+import { ResourceEntity } from './resource.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ResourceEntity])],
   providers: [ResourceService],
-  controllers: [ResourceController]
+  controllers: [ResourceController],
 })
 export class ResourceModule {}
