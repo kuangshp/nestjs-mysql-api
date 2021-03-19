c7a8f1130fe6b6f04a910edb0b3afe760196b248
kuangshp <kuangshp@126.com>
2021-03-19 17:51:27 +0800

    删除文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
