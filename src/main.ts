3912e1fac65ff61426c77dc94d6377a96a98da91
kuangshp <kuangshp@126.com>
2021-03-19 17:44:47 +0800

    新增代码

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log('当前环境', process.env.npm_lifecycle_event);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
