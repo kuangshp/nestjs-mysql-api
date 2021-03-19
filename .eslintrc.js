da10db2dc463068a8d2b60540c3728633068bda4
kuangshp <kuangshp@126.com>
2021-03-19 17:58:33 +0800

    新增基础配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,34 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint',
+    '@typescript-eslint/eslint-plugin',
+    'prettier'
+  ],
   extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint'
   ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+    'prettier/prettier': 'off',
+    'semi': ['error', 'always'],
+    'max-len': 0,
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-misused-promises': 'off'
+  }
+}
