50ed8c13312f448980c79c89d796c4c7f99ebc72
kuangshp <kuangshp@126.com>
2021-03-23 15:57:17 +0800

    资源数据模型

---
+++ src/modules/admin/system/access/entities/access.entity.ts
@@ -0,0 +1,86 @@
+import { Column, Entity } from 'typeorm';
+import { PublicEntity } from '@src/modules/shared/entities/public.entity';
+
+@Entity('access')
+export class AccessEntity extends PublicEntity {
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 50,
+    name: 'module_name',
+    comment: '模块名称'
+  })
+  moduleName: string;
+
+  @Column({
+    type: 'tinyint',
+    nullable: true,
+    name: 'type',
+    comment: '类型,1:表示模块,2:表示菜单,3:表示接口'
+  })
+  type: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 100,
+    name: 'action_name',
+    comment: '操作名称'
+  })
+  actionName: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 100,
+    name: 'icon',
+    comment: '小图标'
+  })
+  icon: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 100,
+    name: 'url',
+    comment: 'url地址'
+  })
+  url: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 10,
+    name: 'method',
+    comment: '请求方式'
+  })
+  method: string;
+
+  @Column({
+    type: 'int',
+    nullable: false,
+    default: () => -1,
+    name: 'module_id',
+    comment: '父模块id'
+  })
+  moduleId: number;
+
+  @Column({
+    type: 'int',
+    nullable: false,
+    default: () => 1,
+    name: 'sort',
+    comment: '排序'
+  })
+  sort: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+    length: 100,
+    name: 'description',
+    comment: '描素'
+  })
+  description: string;
+}
