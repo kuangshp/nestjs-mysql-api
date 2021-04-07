0b31851c076f319428d1d7ebd0ad20663d69039f
kuangshp <kuangshp@126.com>
2021-04-07 17:46:46 +0800

    fix: 修复菜单接口异常问题

--- src/modules/admin/system/access/controllers/menus/menus.controller.ts
+++ src/modules/admin/system/access/controllers/menus/menus.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, UseGuards, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, UseGuards, HttpCode, HttpStatus, Get } from '@nestjs/common';
 import { ApiTags, ApiBearerAuth, ApiOperation, ApiCreatedResponse } from '@nestjs/swagger';
 import { AuthGuard } from '@src/guard/auth/auth.guard';
 import adminConfig from '@src/config/admin.config';
@@ -11,23 +11,20 @@ import { CurrentUser, ICurrentUserType } from '@src/decorators/current.user';
 @UseGuards(AuthGuard)
 @Controller(`${adminConfig.adminPath}/menus`)
 export class MenusController {
-  constructor (
-    private readonly menusService: MenusService,
-  ) { }
+  constructor(private readonly menusService: MenusService) {}
 
   @ApiOperation({
     summary: '获取菜单列表',
-    description: '获取菜单'
+    description: '获取菜单',
   })
   @ApiCreatedResponse({
     type: MenusListResDto,
     isArray: true,
-    description: '获取菜单返回值'
+    description: '获取菜单返回值',
   })
   @HttpCode(HttpStatus.OK)
-  async menusList(
-    @CurrentUser() userInfo: ICurrentUserType,
-  ): Promise<MenusListResDto[]> {
+  @Get()
+  async menusList(@CurrentUser() userInfo: ICurrentUserType): Promise<MenusListResDto[]> {
     return await this.menusService.menusList(userInfo);
   }
 }
