22a9a4178ce71631ab7f0653177f2f3277b240aa
kuangshp <kuangshp@126.com>
2021-04-07 16:47:09 +0800

    feat: 权限返回菜单

--- src/modules/admin/system/access/services/menus/menus.service.ts
+++ src/modules/admin/system/access/services/menus/menus.service.ts
@@ -5,14 +5,18 @@ import { Repository, getConnection } from 'typeorm';
 import { MenusListResDto } from '../../controllers/menus/dto/menus.res.dto';
 import { ICurrentUserType } from '@src/decorators/current.user';
 import { AccountEntity } from '../../../account/entities/account.entity';
-import { AdminIdentityEnum } from '@src/enums';
+import { AdminIdentityEnum, AccessTypeEnum } from '@src/enums';
+import { AccountRoleEntity } from '../../../account/entities/account.role.entity';
+import { RoleAccessEntity } from '../../../role/entities/role.access.entity';
 
 @Injectable()
 export class MenusService {
-  constructor (
+  constructor(
     @InjectRepository(AccessEntity)
     private readonly accessRepository: Repository<AccessEntity>,
-  ) { }
+    @InjectRepository(AccountRoleEntity)
+    private readonly accountRoleRepository: Repository<AccountRoleEntity>,
+  ) {}
 
   /**
    * @Author: 水痕
@@ -29,16 +33,65 @@ export class MenusService {
      * 2.如果是超级管理员就返回全部菜单
      * 3.非超级管理员，根据当前用户拥有的角色去查询(角色资源)表获取全部的资源
      */
-    const { id } = userInfo;
-    const accountInfo: AccountEntity | undefined = await getConnection().createQueryBuilder(AccountEntity, 'account')
-      .select([])
-      .addSelect('account.isSuper', 'isSuper')
-      .where('account.id = :id', { id })
-      .getRawOne();
-    // 1.查询全部的菜单
-    const accessList: AccessEntity[] = await this.accessRepository.find({ where: [{ type: 1 }, { type: 2 }] });
-    // 1.1格式化菜单
-    const formatMenus: MenusListResDto[] = accessList.map((item: AccessEntity) => {
+    const { id, isSuper } = userInfo;
+    // 超级管理员就全部返回
+    if (Object.is(isSuper, AdminIdentityEnum.SUPPER)) {
+      // 1.查询全部的菜单(模块和菜单)
+      const accessList: AccessEntity[] = await this.accessRepository.find({
+        where: [{ type: AccessTypeEnum.MODULE }, { type: AccessTypeEnum.MENUS }],
+      });
+      // 1.1格式化菜单
+      return this.formatMenus(accessList);
+    } else {
+      // 2.根据当前账号id查询已经授权的角色
+      const authRoleList: AccountRoleEntity[] = await this.accountRoleRepository.find({
+        where: { accountId: id },
+        select: ['roleId'],
+      });
+      const authRoleIdList: number[] = authRoleList.map((item: AccountRoleEntity) => item.roleId);
+      console.log(authRoleList, '授权的角色列表', authRoleIdList);
+      // 3.根据角色ID列表获取当前账号拥有的资源id
+      const authAccessList: RoleAccessEntity[] = await getConnection()
+        .createQueryBuilder(RoleAccessEntity, 'role_access')
+        .select(['role_access.accessId'])
+        .where('role_access.roleId in (:...roleId) and role_access.type = 2', {
+          roleId: authRoleIdList,
+        })
+        .getMany();
+      console.log(authAccessList, '授权的资源列表'); // [ RoleAccessEntity { accessId: 5 } ]
+      const authAccessIdList: number[] = authAccessList.map(
+        (item: RoleAccessEntity) => item.accessId,
+      );
+      // 4.根据资源id去查询菜单并且格式化返回
+      const accessList = await getConnection()
+        .createQueryBuilder(AccessEntity, 'access')
+        .select([
+          'access.id',
+          'access.moduleName',
+          'access.actionName',
+          'access.parentId',
+          'access.url',
+          'access.sort',
+          'access.icon',
+        ])
+        .where('(access.id in (:...authAccessIdList) and (access.type = 1 or access.type = 2))', {
+          authAccessIdList,
+        })
+        .getMany();
+      return this.formatMenus(accessList);
+    }
+  }
+
+  /**
+   * @Author: 水痕
+   * @Date: 2021-04-07 16:45:11
+   * @LastEditors: 水痕
+   * @Description: 格式化返回菜单
+   * @param {AccessEntity} accessList
+   * @return {*}
+   */
+  private formatMenus(accessList: AccessEntity[]): MenusListResDto[] {
+    return accessList.map((item: AccessEntity) => {
       const { id, moduleName, actionName, parentId, url, sort, icon } = item;
       return {
         id,
@@ -46,14 +99,8 @@ export class MenusService {
         parentId,
         url,
         sort,
-        icon
+        icon,
       };
     });
-    // 超级管理员就全部返回
-    if (Object.is(accountInfo?.isSuper, AdminIdentityEnum.SUPPER)) {
-      return formatMenus;
-    }
-    // 2.根据当前账号ID查询分配的角色-->根据已分配的角色ID查询角色资源表获取全部的资源
-    return formatMenus;
   }
 }
