f362fb49c5f698d05f11c68314b141da54ed5364
kuangshp <kuangshp@126.com>
2021-08-29 09:59:18 +0800

    fix: 修复url传递token的时候new URL解析错误

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -9,7 +9,7 @@ import { URL } from 'url';
  * @return {*}
  */
 export const getUrlQuery = (urlPath: string, key: string): string | null => {
-  const url = new URL(urlPath);
+  const url = new URL(urlPath, 'https://www.');
   const params = new URLSearchParams(url.search.substring(1));
   return params.get(key);
 };
---
+++ test/test1.js
@@ -0,0 +1,1 @@
+console.log(new URL('/api/v1/admin?name=1'));
